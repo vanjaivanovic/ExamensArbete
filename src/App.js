4c1c79299cfaf9744468502472df2715679ae68e
Vanja Ivanovic <vanja.i@hotmail.com>
2019-01-17 20:37:02 +0100

    changes in router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import './Assets/Styles/Keyframes.css';
 
-import { Route } from 'react-router-dom';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './Components/Routes/Home.js';
 import Menu from './Components/Routes/Menu.js';
@@ -15,12 +15,15 @@ class App extends Component {
   render(){
     return(
        <div>
-           <Route path="/" component={Home} exact/>
-           <Route path="/Menu" component={Menu} />
-           <Route path="/About" component={About} />
-           <Route path="/Contact" component={Contact} />
-           <Route path="/Gdpr" component={GDPR} />
-
+         <BrowserRouter>
+          <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/Menu" component={Menu} />
+             <Route path="/About" component={About} />
+             <Route path="/Contact" component={Contact} />
+             <Route path="/Gdpr" component={GDPR} />
+           </Switch>
+         </BrowserRouter>
        </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -8,10 +8,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(
-  <BrowserRouter>
-    < App />
-  </BrowserRouter> , document.getElementById('root'));
+ReactDOM.render( < App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
